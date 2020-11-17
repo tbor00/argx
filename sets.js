cdf9b7d907a4d77832f8bdca22d831b9a44c5c9b
tbor00 <btimoteo@outlook.com>
2020-11-17 17:16:39 -0300

    edit

--- sets.js
+++ sets.js
@@ -3,7 +3,7 @@ gads.addEventListener("click", function () {
   Swal.fire({
     title: `<i class="icofont-brand-google" style="color:red; font-size: 3em;"></i> `,
     html:
-      "Google Ads es un servicio y un programa de la empresa Google que se utiliza para ofrecer publicidad patrocinada a potenciales anunciantes. Los anuncios patrocinados de Google Ads aparecen en: En la página de resultados junto con los resultados de búsquedas naturales u orgánicas" +
+      "Google Ads es un servicio y un programa de la empresa Google que se utiliza para ofrecer publicidad patrocinada a potenciales anunciantes. Los anuncios patrocinados de Google Ads aparecen en: En la página de resultados junto con los resultados de búsquedas naturales u orgánicas." +
       `<a href="https://es.wikipedia.org/wiki/Google_Ads" target="__blank" style="outline:none"> Info</a>`,
     focusConfirm: false,
     confirmButtonText: "Okey",
@@ -27,7 +27,7 @@ cp.addEventListener("click", function () {
   Swal.fire({
     title: `<i class="icofont-globe" style="color:red; font-size: 3em;"></i> `,
     html:
-      "La voz fue adoptada por la industria de la publicidad con el significado de un plan extenso para una serie de anuncios diferentes, pero relacionados, que aparecen en diversos medios durante un periodo específico" +
+      "La voz fue adoptada por la industria de la publicidad con el significado de un plan extenso para una serie de anuncios diferentes, pero relacionados, que aparecen en diversos medios durante un periodo específico." +
       `<a href="https://es.wikipedia.org/wiki/Campa%C3%B1a_publicitaria" target="__blank" style="outline:none"> Info</a>`,
     focusConfirm: false,
     confirmButtonText: "Okey",
