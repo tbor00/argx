0a430d07cda11d0b2241485d8e875b052e0b0b5a
tbor00 <btimoteo@outlook.com>
2020-11-17 17:14:46 -0300

    edit info

--- sets.js
+++ sets.js
@@ -4,7 +4,7 @@ gads.addEventListener("click", function () {
     title: `<i class="icofont-brand-google" style="color:red; font-size: 3em;"></i> `,
     html:
       "Google Ads es un servicio y un programa de la empresa Google que se utiliza para ofrecer publicidad patrocinada a potenciales anunciantes. Los anuncios patrocinados de Google Ads aparecen en: En la página de resultados junto con los resultados de búsquedas naturales u orgánicas" +
-      `<a href="https://es.wikipedia.org/wiki/Google_Ads" target="__blank" style="outline:none"> Para más info click acá</a>`,
+      `<a href="https://es.wikipedia.org/wiki/Google_Ads" target="__blank" style="outline:none"> Info</a>`,
     focusConfirm: false,
     confirmButtonText: "Okey",
   });
@@ -16,7 +16,7 @@ mkt.addEventListener("click", function () {
     title: `<i class="icofont-stock-mobile" style="color:red; font-size: 3em;"></i> `,
     html:
       "La mercadotecnia digital o marketing digital está caracterizada por la combinación y utilización de estrategias de comercialización en medios digitales." +
-      `<a href="https://es.wikipedia.org/wiki/Marketing_digital" target="__blank" style="outline:none"> Para más info click acá</a>`,
+      `<a href="https://es.wikipedia.org/wiki/Marketing_digital" target="__blank" style="outline:none"> Info</a>`,
     focusConfirm: false,
     confirmButtonText: "Okey",
   });
@@ -28,7 +28,7 @@ cp.addEventListener("click", function () {
     title: `<i class="icofont-globe" style="color:red; font-size: 3em;"></i> `,
     html:
       "La voz fue adoptada por la industria de la publicidad con el significado de un plan extenso para una serie de anuncios diferentes, pero relacionados, que aparecen en diversos medios durante un periodo específico" +
-      `<a href="https://es.wikipedia.org/wiki/Campa%C3%B1a_publicitaria" target="__blank" style="outline:none"> Para más info click acá</a>`,
+      `<a href="https://es.wikipedia.org/wiki/Campa%C3%B1a_publicitaria" target="__blank" style="outline:none"> Info</a>`,
     focusConfirm: false,
     confirmButtonText: "Okey",
   });
